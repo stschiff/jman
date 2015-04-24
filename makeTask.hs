4daaa74cd7db08bc0636f8fedb830ec464b4a9da
Stephan Schiffels <stephan.schiffels@mac.com>
2015-04-24 19:05:16 +0200

    added makeProject, compiles

--- makeTask.hs
+++ makeTask.hs
@@ -9,9 +9,9 @@ import qualified Data.ByteString.Lazy.Char8 as B
 
 data Options = Options {
     _optName :: String,
+    _optCommand :: String,
     _optInputFiles :: [FilePath],
     _optOutputFiles :: [FilePath],
-    _optCommand :: String,
     _optMem :: Int,
     _optThreads :: Int,
     _optFarmQueue :: String,
@@ -25,27 +25,26 @@ main = OP.execParser optParser >>= runWithOptions
                 (OP.fullDesc <> OP.progDesc "A utility to generate a JSON encoded task for the tman program")
 
 options :: OP.Parser Options
-options = Options <$> OP.strOption (OP.long "name" <> OP.short 'n' <> OP.metavar "<NAME>" <>
+options = Options <$> OP.strArgument (OP.metavar "<NAME>" <>
                                       OP.help "full hierarchical name of the command")
-                    <*> many (OP.strOption (OP.long "input" <> OP.short 'i' <>
-                              OP.metavar "<File1>" <> OP.help "input file, can be given multiple times"))
-                    <*> many (OP.strOption (OP.long "output" <> OP.short 'o' <>
-                              OP.metavar "<File1>" <> OP.help "output file, can be given multiple times"))
-                    <*> OP.strOption (OP.long "command" <> OP.short 'c' <> OP.metavar "<CMD>" <>
-                                      OP.help "full bash command line")
-                    <*> OP.option OP.auto (OP.long "mem" <> OP.short 'm' <> OP.metavar "<Mb>" <>
-                                           OP.value 100 <> OP.showDefault <>
-                                           OP.help "maximum memory in Mb (only for LSF submission)")
-                    <*> OP.option OP.auto (OP.long "threads" <> OP.short 't' <> OP.metavar "<nrThreads>" <>
-                                           OP.value 1 <> OP.showDefault <>
-                                           OP.help "nr of threads (only for LSF submission)")
-                    <*> OP.strOption (OP.long "queue" <> OP.short 'q' <> OP.metavar "<Queue>" <>
-                                      OP.value "normal" <> OP.showDefault <>
-                                      OP.help "queue name (only for LSF submission)")
-                    <*> OP.strOption (OP.long "group" <> OP.short 'g' <> OP.metavar "<Farm-Group>" <>
-                                      OP.value "" <> OP.showDefault <>
-                                      OP.help ("Farm group, uses Environment variable $LSF_DEFAULTGROUP if left" ++
-                                               " blank (only for LSF submission)"))
+                  <*> OP.strArgument (OP.metavar "<CMD>" <> OP.help "full bash command line")
+                  <*> many (OP.strOption (OP.long "input" <> OP.short 'i' <>
+                            OP.metavar "<File>" <> OP.help "input file, can be given multiple times"))
+                  <*> many (OP.strOption (OP.long "output" <> OP.short 'o' <>
+                            OP.metavar "<File>" <> OP.help "output file, can be given multiple times"))
+                  <*> OP.option OP.auto (OP.long "mem" <> OP.short 'm' <> OP.metavar "<Mb>" <>
+                                         OP.value 100 <> OP.showDefault <>
+                                         OP.help "maximum memory in Mb (only for LSF submission)")
+                  <*> OP.option OP.auto (OP.long "threads" <> OP.short 't' <> OP.metavar "<nrThreads>" <>
+                                         OP.value 1 <> OP.showDefault <>
+                                         OP.help "nr of threads (only for LSF submission)")
+                  <*> OP.strOption (OP.long "queue" <> OP.short 'q' <> OP.metavar "<Queue>" <>
+                                    OP.value "normal" <> OP.showDefault <>
+                                    OP.help "queue name (only for LSF submission)")
+                  <*> OP.strOption (OP.long "group" <> OP.short 'g' <> OP.metavar "<Farm-Group>" <>
+                                    OP.value "" <> OP.showDefault <>
+                                    OP.help ("Farm group, uses Environment variable $LSF_DEFAULTGROUP if left" ++
+                                             " blank (only for LSF submission)"))
 
 runWithOptions :: Options -> IO ()
 runWithOptions opts = runScript $ do
@@ -53,4 +52,4 @@ runWithOptions opts = runScript $ do
     scriptIO . B.putStrLn . encode $ task
 
 makeTask :: Options -> Either String Task
-makeTask (Options n i o c m t q g) =  return $ Task n i o c m t q g
+makeTask (Options n c i o m t q g) =  return $ Task n i o c m t q g
