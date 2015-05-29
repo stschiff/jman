6fe4aa72363b6d43d7e3ea80c17c134ecddd6580
Stephan Schiffels <stephan.schiffels@mac.com>
2015-05-29 22:36:29 +0100

    m

--- TmanTask.hs
+++ TmanTask.hs
@@ -4,6 +4,7 @@ module TmanTask (task,
                  outputFiles,
                  mem,
                  nrThreads,
+                 (&),
                  submissionQueue,
                  submissionGroup,
                  Task) where
@@ -37,6 +38,6 @@ submissionQueue q task = task {_tSubmissionQueue = unpack q}
 submissionGroup :: Setter Text
 submissionGroup g task = task {_tSubmissionGroup = unpack g}
 
--- (&) :: a -> (a -> b) -> b
--- a & f = f a
--- infixl 1 &
+(&) :: a -> (a -> b) -> b
+a & f = f a
+infixl 1 &
